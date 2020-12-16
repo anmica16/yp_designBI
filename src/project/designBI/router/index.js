2c572ae43c6897bc04db1ead9ef1408f44caf5c9
YunPeng <anmica@sina.cn>
2020-12-16 21:52:25 +0800

    阿里云皮肤框架

--- src/project/designBI/router/index.js
+++ src/project/designBI/router/index.js
@@ -11,6 +11,14 @@ import UserPage from "../views/Page/UserPage/UserPage.vue";
 import DesignCenter from "../views/Page/UserPage/DesignCenter/DesignCenter.vue";
 import DesignEdit from "../views/Page/UserPage/DesignEdit/DesignEdit.vue";
 
+//--------
+// 阿里云皮肤
+//--------
+import aliLogin from "../views/Page_ali/Login.vue";
+import aliMain from "../views/Page_ali/Main.vue";
+import aliMyPage from "../views/Page_ali/main/MyPage.vue";
+import aliWorkSpace from "../views/Page_ali/main/WorkSpace.vue";
+
 const routes = [
   {
     path: "/",
@@ -36,6 +44,28 @@ const routes = [
             path: "edit/:templateCode",
             name: "DesignEdit",
             component: DesignEdit
+          }
+        ]
+      },
+      {
+        path: "ali/login",
+        name: "aliLogin",
+        component: aliLogin
+      },
+      {
+        path: "ali/main",
+        name: "aliMain",
+        component: aliMain,
+        children: [
+          {
+            path: "mypage",
+            name: "aliMyPage",
+            component: aliMyPage
+          },
+          {
+            path: "work",
+            name: "aliWorkSpace",
+            component: aliWorkSpace
           }
         ]
       }
---
+++ src/project/designBI/views/Page_ali/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>aliLogin</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
