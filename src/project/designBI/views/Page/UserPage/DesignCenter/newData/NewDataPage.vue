e2fc28c8cb92e34fe6eec174adce397c2fd3d2e3
anmica16 <anmica@sina.cn>
2020-12-24 10:33:50 +0800

    拖拽文件上传 part1

--- src/project/designBI/views/Page/UserPage/DesignCenter/newData/NewDataPage.vue
+++ src/project/designBI/views/Page/UserPage/DesignCenter/newData/NewDataPage.vue
@@ -7,13 +7,18 @@
       </div>
       <div class="fill"></div>
       <div class="yesno">
-        <el-button ref="cancel" type="info" :disabled="!canCancel"
+        <el-button
+          ref="cancel"
+          type="info"
+          :disabled="!canCancel"
+          @click="cancelFn"
           >取消</el-button
         >
         <el-button
           ref="submit"
           :type="canSubmit ? 'primary' : ''"
           :disabled="!canSubmit"
+          @click="submitFn"
           >确定</el-button
         >
       </div>
@@ -35,7 +40,12 @@
             <el-button type="primary" v-if="workBook">重新上传</el-button>
           </div>
         </div>
-        <div class="bodyInfo">
+        <div
+          class="bodyInfo"
+          @dragover.stop.prevent="leftDragOverFn"
+          @dragleave="leftDragLeaveFn"
+          @drop.stop.prevent="leftDrogFn"
+        >
           <div class="tipText">
             <template v-if="!workBook">
               <div class="text1">要求：</div>
@@ -58,9 +68,19 @@
               <div class="text1">第一行为字段名，第二行开始为字段的值</div>
             </template>
           </div>
+          <!-- ~ 1  -->
           <template v-if="workBook">
             <div class="fieldType"></div>
           </template>
+          <!-- ~ 2 覆盖全部body的 drop层 -->
+          <div
+            v-show="leftDragOver"
+            class="dragTip"
+            :class="{ newUpd: !workBook }"
+          >
+            <div class="icon"></div>
+            <div class="text">{{ leftDragText }}</div>
+          </div>
         </div>
       </div>
       <div class="rightArea">
@@ -77,6 +97,8 @@
 </template>
 
 <script>
+import $ from "@/plugins/js/loader";
+import Vue from "vue";
 export default {
   name: "NewDataPage",
   props: {
@@ -102,15 +124,100 @@ export default {
       name: "",
       canSubmit: false,
       canCancel: true,
+      leftDragOver: false,
       //@ 4 上传读取的数据
+      reading: false,
       workBook: null
     };
   },
+  computed: {
+    rABS() {
+      return (
+        typeof FileReader !== "undefined" &&
+        (FileReader.prototype || {}).readAsBinaryString
+      );
+    },
+    leftDragText() {
+      return this.workBook
+        ? "拖拽表格到此处以上传"
+        : "拖拽表格到此处以重新上传";
+    }
+  },
   methods: {
+    backPage() {
+      this.$router.push({});
+    },
     //~ 2 保存后就取消该 id的 readyAdd状态
-    submitFn() {},
+    submitFn() {
+      let me = this;
+      console.log(["尝试提交", me]);
+    },
     //~ 3 取消则直接删除该record
-    cancelFn() {}
+    cancelFn() {
+      let me = this;
+      //# 1 删除
+      $.ajax({
+        url: Vue.Api.designBI,
+        method: Vue.Api.designBI.Delete,
+        data: {
+          table: "data",
+          ids: JSON.stringify([me.id])
+        }
+      }).catch(r => {});
+      //# 2 返回
+      me.backPage();
+    },
+    //~ 5 核心 读取文件，返回res的 wb
+    dealFile(files) {
+      let me = this,
+        X = me.X,
+        rABS = me.rABS;
+      me.reading = true;
+      return new Promise((res, rej) => {
+        var f = files[0];
+        //【update】看能不能自动转换？ie9
+        var reader = new FileReader();
+        //# 1 经过时间处理
+        reader.onload = function(e) {
+          me.leftDragOver = false;
+          var data = e.target.result;
+          if (!rABS) data = new Uint8Array(data);
+          //# 2 获取
+          let wb = X.read(data, { type: rABS ? "binary" : "array" });
+          me.reading = false;
+          console.log(["读取完毕", wb]);
+          res(wb);
+        };
+        if (rABS) reader.readAsBinaryString(f);
+        else reader.readAsArrayBuffer(f);
+      });
+    },
+    //~ 4 drop相关
+    leftDragOverFn(e) {
+      let me = this;
+      me.leftDragOver = true;
+      if (e.dataTransfer) {
+        e.dataTransfer.dropEffect = "copy";
+      }
+    },
+    leftDragLeaveFn(e) {
+      let me = this;
+      me.leftDragOver = false;
+      if (e.dataTransfer) {
+        e.dataTransfer.dropEffect = "move";
+      }
+    },
+    leftDrogFn(e) {
+      let me = this;
+      //# 1 左侧重新上传
+      if (me.workBook) {
+        // 待补充 删除
+      }
+      //# 2 赋值
+      me.dealFile(e.dataTransfer.files).then(wb => {
+        me.workBook = wb;
+      });
+    }
   },
   created() {
     let me = this;
